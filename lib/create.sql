49febc2ce0627933f1c8a033fc9e1c2bdc2a2107
David Mieloch <davidmieloch@gmail.com>
2017-03-20 22:45:19 +0000

    Done.

---
+++ lib/create.sql
@@ -0,0 +1,9 @@
+CREATE TABLE bears (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  age INTEGER,
+  gender BIT, -- BIT is for 2 options, CHAR(1) for more than 2 possibly values
+  color TEXT,
+  temperament TEXT,
+  alive BIT
+)
